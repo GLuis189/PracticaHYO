39b48685a0826e9c03baf5e8a4556783a2e77375
100472037@alumnos.uc3m.es <Simpaticas_22>
2023-12-08 14:24:33 +0100

    cambios

--- parte-1/CSPParking.py
+++ parte-1/CSPParking.py
@@ -32,8 +32,8 @@ def leer_archivo():
     # Es una lista con tuplas de la siguiente manera (ID, TSU/TNU, C/X) ID como entero?
     ambulancias = [(int(partes[0]), partes[1], partes[2]) for partes in (elemento.split('-') for elemento in parking[2:])]
     ambulancias_tsu = [ambulancia for ambulancia in ambulancias if ambulancia[1] == 'TSU']
-    # print(ambulancias)
-    # print(ambulancias_tsu)
+    ambulancias = [f"{ambulancia[0]}-{ambulancia[1]}-{ambulancia[2]}" for ambulancia in ambulancias]
+    ambulancias_tsu = [f"{ambulancia[0]}-{ambulancia[1]}-{ambulancia[2]}" for ambulancia in ambulancias_tsu]
     return dimension, cargas, ambulancias, ambulancias_tsu
 
 
@@ -56,38 +56,71 @@ def guardar_soluciones(soluciones, dimension, ambulancias):
                 plaza = solucion[ambulancia[0]]
 
                 # Colocar la ambulancia en la plaza correspondiente en la matriz
-                parking[plaza[0]-1][plaza[1]-1] = '{}-{}-{}'.format(ambulancia[0], ambulancia[1], ambulancia[2])
+                parking[plaza[0]-1][plaza[1]-1] = ambulancia
 
             # Escribir la matriz en el archivo CSV
             for fila in parking:
                 escritor.writerow(fila)
-            escritor.writerow([])
+            #escritor.writerow([])
 
 dimension, cargas, ambulancias, ambulancias_tsu= leer_archivo()
 problem = Problem()
 
 todas_las_plazas = [(i, j) for i in range(1, dimension[0]+1) for j in range(1, dimension[1]+1)]
+
 # Restricción de unicidad: Todo vehículo tiene que tener asignada una plaza y sólo una
 for ambulancia in ambulancias:
-    if ambulancia[2] == 'C':  # Si el vehículo tiene congelador
-        problem.addVariable(ambulancia[0], cargas)  # Solo puede ir a las plazas con conexión eléctrica
-    else:
-        problem.addVariable(ambulancia[0], todas_las_plazas)
+    if ambulancia.split('-')[2] == 'C':  # Si el vehículo tiene congelador
+        problem.addVariable(ambulancia, cargas)  # Solo puede ir a las plazas con conexión eléctrica
 
-#añade una restricción al problema que indica que todas las ambulancias deben tenerplazas diferentes.
-problem.addConstraint(AllDifferentConstraint(), [ambulancia[0] for ambulancia in ambulancias])
+    else:
+        problem.addVariable(ambulancia, todas_las_plazas)
+
+#añade una restricción al problema que indica que todas las ambulancias deben tener plazas diferentes.
+problem.addConstraint(AllDifferentConstraint(), [variable for variable in ambulancias])
+#problem.addConstraint(AllDifferentConstraint(), [ambulancia[0] for ambulancia in ambulancias])
 
 # Restricción de TSU: Un vehículo de tipo TSU no puede tener aparcado por delante, en su misma fila, a ningún otro vehículo excepto si éste es también de tipo TSU
-def restriccion_tsu(*plazas):
-    tsus = [plaza for plaza, ambulancia in zip(plazas, ambulancias) if ambulancia in ambulancias_tsu]
-    for tsu in tsus:
-        if any(plaza[0] == tsu[0] and plaza[1] > tsu[1] and ambulancia not in ambulancias_tsu for plaza in plazas):
+"""def restriccion_tsu_filas(a1, a2):
+        # Obtener información de la ambulancia desde la variable
+        ambulancia_info = variable.split('-')
+        tipo_ambulancia = ambulancia_info[1]
+        fila_ambulancia = int(ambulancia_info[0])
+
+        # Verificar si la ambulancia es de tipo TSU
+        if tipo_ambulancia == 'TSU':
+            # Obtener todas las variables en la misma fila
+            ambulancias_en_misma_fila = [var for var in variables if var != variable and int(var.split('-')[0]) == fila_ambulancia]
+
+            # Verificar que todas las ambulancias en la misma fila sean de tipo TSU
+            if any(var.split('-')[1] != 'TSU' for var in ambulancias_en_misma_fila):
+                return False
+
+    return True"""
+def restriccion_tsu_filas(a1, a2):
+    if a1[0] == a2[0]:
+        if a1[1] < a2[1]:
             return False
     return True
-problem.addConstraint(restriccion_tsu, [ambulancia[0] for ambulancia in ambulancias])
 
-# Restricción de maniobrabilidad: Todo vehículo debe tener libre una plaza a izquierda o derecha (mirando en dirección a la salida)
-def restriccion_maniobrabilidad(*plazas):
+# Agregar la restricción personalizada al problema
+for a1 in ambulancias:
+    if a1.split('-')[1] == 'TSU':
+        for a2 in ambulancias:
+            if a1 != a2 and a2.split('-')[1]!='TSU':
+                problem.addConstraint(restriccion_tsu_filas, (a1, a2))
+
+"""def restriccion_tsu(*plazas):
+    tsus = [plaza for plaza, ambulancia in zip(plazas, ambulancias) if ambulancia in ambulancias_tsu]
+    for tsu in tsus:
+        if any(plaza[0] == tsu[0] and plaza[1] > tsu[1] and ambulancia not in ambulancias_tsu for plaza, ambulancia in zip(plazas, ambulancias)):
+            return False
+    return True
+problem.addConstraint(restriccion_tsu, [ambulancia[0] for ambulancia in ambulancias])"""
+
+
+# Restricción de maniobrabilidad: Todo vehículo debe tener libre una plaza a izquierda o derecha (mirando en dirección a la salida)"""
+"""def restriccion_maniobrabilidad(*plazas):
     for plaza in plazas:
         # Comprobar si hay ambulancias tanto arriba como abajo
         if any(plaza[1] == otra_plaza[1] and abs(plaza[0] - otra_plaza[0]) == 1 for otra_plaza in plazas if otra_plaza != plaza):
@@ -98,7 +131,26 @@ def restriccion_maniobrabilidad(*plazas):
                  # Si la plaza está en el límite del parking, no considerar la pared como una plaza libre
                 return False
     return True
-problem.addConstraint(restriccion_maniobrabilidad, [ambulancia[0] for ambulancia in ambulancias])
+problem.addConstraint(restriccion_maniobrabilidad, [ambulancia[0] for ambulancia in ambulancias])"""
+def constraint_maniobrabilidad(a1, a2):
+    #Función que comprueba que todo vehículo debe tener libre una plaza a izquierda o derecha.
+    if a1[1] == a2[1]:
+        if (a1[0] == "1"  and a2[0] == str(int(a1[0]) + 1)):
+            return False
+        elif (a1[0] == str(dimension[0]) and a2[0] == str(int(a1[0]) - 1)):
+            return False
+        elif (a2[0] == "1"  and a1[0] == str(int(a2[0]) + 1)):
+            return False
+        elif (a2[0] == str(dimension[0]) and a1[0] == str(int(a2[0]) - 1)):
+            return False
+        elif a2[0] == str(int(a1[0]) + 1) or a2[0] == str(int(a1[0]) - 1):
+            return False
+    return True
+ # Por cuestiones de maniobrabilidad dentro del parking todo vehículo debe tener libre una plaza a izquierda o derecha (mirando en dirección a la salida).
+for a1 in ambulancias:
+    for a2 in ambulancias:
+        if a1 != a2:
+            problem.addConstraint(constraint_maniobrabilidad, (a1, a2))
 
 soluciones = problem.getSolutions()
 num_soluciones = len(soluciones)
@@ -107,4 +159,5 @@ if num_soluciones == 0:
     exit()
 
 # Llamar a la función guardar_soluciones con las soluciones obtenidas
-guardar_soluciones(soluciones, dimension, ambulancias)
+print(soluciones)
+#guardar_soluciones(soluciones, dimension, ambulancias)
